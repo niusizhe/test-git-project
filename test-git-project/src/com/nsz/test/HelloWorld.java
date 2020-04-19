5c997fd3913ce391bb1a6418c65d7e292233abf5
shantianyounaizi <296337125@qq.com>
2020-04-19 22:55:10 +0800

    提交test-git-project

---
+++ test-git-project/src/com/nsz/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.nsz.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
